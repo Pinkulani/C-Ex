bd8e519603eeae7de6bc074b763a70b4c6aafda6
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-06-27 08:22:44 +0200

    Add files via upload

---
+++ Battleships.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int Setup() {
+    int Field[10][10];
+
+    return -1;
+}
+
+
+
+int GameRules() {
+	std::cout << "|- Battleships -| \n"; std::cout << "All Ships are 1 space big \n"; std::cout << "Game field is 10x10 big \n";
+    return -1;
+}
+
+
+int main() {
+    GameRules();
+    return -1;
+}
+
+int Shoot() {
+    return -1;
+}
+
+int ValidateShot() {
+    return -1;
+}
+
+
+
