f7dfbf0e933435e6dfbed7f5e32daee45e1eeff2
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-07-09 02:36:11 +0200

    Add files via upload

---
+++ MathTrigonometry.cpp
@@ -0,0 +1,45 @@
+// Emilia @ 9.7.2023
+
+#include <iostream>
+using namespace std;
+
+int A;
+int B;
+int C;
+
+int AB; // Hypotenuse
+int BC;
+int CA;
+
+void Input() {
+    cout << "AB Length: " << endl;
+    cin >> AB;
+    cout << "BC Length: " << endl;
+    cin >> BC;
+    cout << "CA Length: " << endl;
+    cin >> CA;
+}
+
+void Explanation() {
+    cout << "Explanations: " << endl << "AB is your Hypotenuse" << endl << "----------" << endl;
+}
+
+int Sine() {
+    
+}
+
+int Cosine() {
+    
+}
+
+int Tangent() {
+    
+}
+
+
+
+int main() {
+    Explanation();
+    Input();
+    return -1;
+}
